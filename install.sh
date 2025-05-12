f5bb65534fb52121ed6373406e1bbdead65eee56
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-05-12 13:45:05 -0600

    Pin to Mesa upstream 25.1.0 and use a patch instead of cloning a branch

--- install.sh
+++ install.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 
+MODE=0
 REALHOME="$(realpath "$HOME")"
 ARTIFACTDIR="$(realpath "./output")"
+PKGNAME="fedora-mesa-git"
+PKGDIR="$(realpath "./artifact")"
 
+UDD="$(which update-desktop-database)"
 TARGETDIR="$REALHOME/mesa"
 LOCAL_APPS="$REALHOME/.local/share/applications"
+
+show_help() {
+	cat <<EOF
+Usage: $0 [OPTIONS]
+
+Install or package custom Mesa build.
+
+Options:
+  -h, --help      Show this help message and exit
+  -i, --install   Install Mesa to user's home directory (default)
+  -p, --package   Create a compressed package of Mesa artifacts
+EOF
+}
 
 confirm() {
 	read -r -p "$1 (Y/n) " response
@@ -16,24 +33,69 @@ confirm() {
 	fi
 }
 
+package_artifacts() {
+	local pkgfile
+	pkgfile="${PKGDIR}/${PKGNAME}-$(date +%m%d%Y).tar.gz"
+
+	echo "Packaging Mesa artifacts..."
+	rm -rf "$PKGDIR" && mkdir -p "$PKGDIR"
+	tar -czf "$pkgfile" -C "$ARTIFACTDIR" .
+	echo "Success! Created package: $pkgfile"
+}
+
+copy_artifacts() {
+	local outdir=$1
+	rm -rf "$outdir"
+	mkdir -p "$outdir"/
+	cp -f ./extras/{mesa-run.sh,steam-wrapped.sh,steam-wrapped.desktop} "$outdir"/
+	cp -rf "$ARTIFACTDIR"/* "$outdir"/
+	# make links for system-wide usage
+	sudo ln -sf "$outdir"/mesa-run.sh /usr/local/bin/mesa-run.sh
+	sudo ln -sf "$outdir"/steam-wrapped.sh /usr/local/bin/steam-wrapped.sh
+	# copy .desktop file for wrapped Steam and run update-desktop-database
+	cp -f "$outdir"/steam-wrapped.desktop "$LOCAL_APPS"/steam-wrapped.desktop
+	"$UDD" "$LOCAL_APPS" && echo "Ran 'update-desktop-database' successfully..."
+}
+
+install_mesa() {
+	if [ "$MODE" -eq 1 ]; then
+		[ -d "$TARGETDIR" ] &&
+			! confirm "Warning: '$TARGETDIR' already exists, continuing will overwrite it!" && exit 1
+		copy_artifacts "$TARGETDIR"
+	elif [ "$MODE" -eq 2 ]; then
+		[ -d "$PKGDIR" ] &&
+			! confirm "Warning: '$PKGDIR' already exists, continuing will overwrite it!" && exit 1
+		copy_artifacts "$PKGDIR"
+	fi
+
+	echo "Success! Use the 'Steam (wrapped)' shortcut or 'mesa-run.sh' command"
+}
+
 if [ -z "$ARTIFACTDIR" ] || ! [ -d "$ARTIFACTDIR" ]; then
 	echo "Error: could not find Mesa build artifact directory '$ARTIFACTDIR'!"
 	exit 1
 fi
 
-if [ -d "$TARGETDIR" ]; then
-	! confirm "Warning: '$TARGETDIR' already exists, continuing will overwrite it!" && exit 1
-fi
-
-chmod 0755 ./extras/mesa-run.sh ./extras/steam-wrapped.sh
-sudo cp -f ./extras/steam-wrapped.sh /usr/local/bin/
-sudo ln -sf "$REALHOME"/mesa/mesa-run.sh /usr/local/bin/mesa-run.sh
-
-cp -f ./extras/steam-wrapped.desktop "$LOCAL_APPS"/ &&
-	update-desktop-database "$LOCAL_APPS"
-
-if sudo chown -R "$USER":"$USER" "$ARTIFACTDIR" &&
-	mkdir -p "$TARGETDIR"/ && cp -rf "$ARTIFACTDIR"/* "$TARGETDIR"/ &&
-	cp -f ./extras/mesa-run.sh "$TARGETDIR"/; then
-	echo "Success! Use the 'Steam (wrapped)' shortcut or 'mesa-run.sh'..."
+MODE=1
+if [[ $# -eq 0 ]]; then
+	install_mesa # default to "-i" behavior
+else
+	case "$1" in
+	-h | --help)
+		show_help
+		exit 0
+		;;
+	-i | --install)
+		install_mesa
+		;;
+	-p | --package)
+		MODE=2
+		install_mesa
+		;;
+	*)
+		echo "Unknown option: $1"
+		show_help
+		exit 1
+		;;
+	esac
 fi
--- run.sh
+++ run.sh
@@ -6,7 +6,9 @@ BUILDDIR="./output"
 
 GIT="$(which git)"
 REPODIR="mesa-git"
-REPO=(--branch radv-float8-hack3 https://gitlab.freedesktop.org/DadSchoorse/mesa.git)
+# REPO=(--branch radv-float8-hack3 https://gitlab.freedesktop.org/DadSchoorse/mesa.git)
+REPO=(https://gitlab.freedesktop.org/mesa/mesa.git)
+REV="5c142e46f3f6e752ed745fd48912ebb8fad67145" # pin to 25.1.0
 
 # prevent script from being run outside the project directory
 script_dir="$(dirname "$(readlink -f "$0")")"
@@ -17,6 +19,13 @@ fi
 
 if [ -n "$GIT" ] && ! [ -d ./"$REPODIR" ]; then
 	"$GIT" clone "${REPO[@]}" "$REPODIR"
+	cd "$REPODIR" || exit 1
+	# reset repo to pinned revision state
+	"$GIT" reset --hard "$REV"
+	"$GIT" clean -fd
+	# apply radv-float8-hack3 patch from DadSchoorse:
+	# https://gitlab.freedesktop.org/DadSchoorse/mesa/-/commit/4823285a2e5b2df849b55861dd4f051dd2598bf1
+	"$GIT" apply <../radv-float8-hack3.patch && cd ../ || exit 1
 fi
 
 chmod 0755 ./entry.sh
