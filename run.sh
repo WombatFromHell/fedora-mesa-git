b79e9e54a458f7a47af2dfdecaf58adfe9166875
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-05-19 09:04:46 -0600

    Fixup radv-float8-hack3 using format-patch and git am

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ GIT="$(which git)"
 REPODIR="mesa-git"
 # REPO=(--branch radv-float8-hack3 https://gitlab.freedesktop.org/DadSchoorse/mesa.git)
 REPO=(https://gitlab.freedesktop.org/mesa/mesa.git)
-REV="5c142e46f3f6e752ed745fd48912ebb8fad67145" # pin to 25.1.0
+# REV="5c142e46f3f6e752ed745fd48912ebb8fad67145" # pin to 25.1.0
+REV="97f71420dfdf86de084b64cbcbd65855063fcc94" # pin to current upstream
 
 # prevent script from being run outside the project directory
 script_dir="$(dirname "$(readlink -f "$0")")"
@@ -25,7 +26,12 @@ if [ -n "$GIT" ] && ! [ -d ./"$REPODIR" ]; then
 	"$GIT" clean -fd
 	# apply radv-float8-hack3 patch from DadSchoorse:
 	# https://gitlab.freedesktop.org/DadSchoorse/mesa/-/commit/4823285a2e5b2df849b55861dd4f051dd2598bf1
-	"$GIT" apply <../radv-float8-hack3.patch && cd ../ || exit 1
+	if ! "$GIT" am --no-gpg-sign ../radv-float8-hack3.patch; then
+		echo "Something went wrong when applying 'radv-float8-hack3.patch'!"
+		exit 1
+	else
+		cd "$script_dir" || exit 1
+	fi
 fi
 
 chmod 0755 ./entry.sh
