fe1b5f5e956753517737e366a140f7affa1cbed2
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-05-21 10:44:42 -0600

    Ensure a full reconfigure for each build and add some optimization flags

--- entry.sh
+++ entry.sh
@@ -4,7 +4,7 @@ cd "/opt/mesa" || exit 1
 OUTDIR="$(realpath ./output)"
 
 MESON="$(which meson)"
-MESON_OPTS=(setup build64 --reconfigure --libdir lib64 --prefix "$OUTDIR" "-Dgallium-drivers=radeonsi,zink" -Dvideo-codecs=all -Dvulkan-drivers=amd -Dbuildtype=release)
+MESON_OPTS=(setup build64 --reconfigure --libdir lib64 --prefix "$OUTDIR" -Dgallium-drivers=radeonsi -Dvideo-codecs=all -Dvulkan-drivers=amd -Dbuildtype=release -Dc_args=-O3 -Dcpp_args=-O3)
 
 NINJA="$(which ninja)"
 NINJA_OPTS=(-C build64 install)
@@ -15,6 +15,8 @@ if [ -z "$MESON" ] || [ -z "$NINJA" ]; then
 fi
 
 cd "mesa-git" || exit 1
+# ensure we build/reconfigure from scratch every time
+rm -rf ./build64 || exit 1
 
 if ! "$MESON" "${MESON_OPTS[@]}"; then
 	echo "Error: something went wrong when configuring meson build!"
--- extras/mesa-run.sh
+++ extras/mesa-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 MESA="$HOME/mesa"
+# D3D_MODULE_PATH="$MESA"/lib64/d3d/d3dadapter9.so.1 \ # Gallium related (deprecated)
 LD_LIBRARY_PATH="$MESA"/lib64:"$LD_LIBRARY_PATH" \
 	VK_ICD_FILENAMES="$MESA"/share/vulkan/icd.d/radeon_icd.x86_64.json \
 	VK_DRIVER_FILES="$MESA"/share/vulkan/icd.d/radeon_icd.x86_64.json \
@@ -8,5 +9,4 @@ LD_LIBRARY_PATH="$MESA"/lib64:"$LD_LIBRARY_PATH" \
 	LIBGL_DRIVERS_PATH="$MESA"/lib64/dri \
 	LIBVA_DRIVERS_PATH="$MESA"/lib64/dri \
 	VDPAU_DRIVER_PATH="$MESA"/lib64/dri \
-	D3D_MODULE_PATH="$MESA"/lib64/d3d/d3dadapter9.so.1 \
 	exec "$@"
