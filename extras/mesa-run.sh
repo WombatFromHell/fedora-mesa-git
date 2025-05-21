cb5a87282bac3225138dc82e7336592a34c827b8
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-05-20 19:50:00 -0600

    Include DRIRC_CONFIGDIR in mesa-run.sh for most recent mesa workarounds

--- extras/mesa-run.sh
+++ extras/mesa-run.sh
@@ -4,6 +4,7 @@ MESA="$HOME/mesa"
 LD_LIBRARY_PATH="$MESA"/lib64:"$LD_LIBRARY_PATH" \
 	VK_ICD_FILENAMES="$MESA"/share/vulkan/icd.d/radeon_icd.x86_64.json \
 	VK_DRIVER_FILES="$MESA"/share/vulkan/icd.d/radeon_icd.x86_64.json \
+	DRIRC_CONFIGDIR="$MESA"/share/drirc.d \
 	LIBGL_DRIVERS_PATH="$MESA"/lib64/dri \
 	LIBVA_DRIVERS_PATH="$MESA"/lib64/dri \
 	VDPAU_DRIVER_PATH="$MESA"/lib64/dri \
