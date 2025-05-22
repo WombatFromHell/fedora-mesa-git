122fb10f8dde118d3d7a25645d8c9ce93a7f88f8
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-05-22 15:26:20 -0600

    Pull from a pinned rev of the rebased fp8-hack repo

--- run.sh
+++ run.sh
@@ -6,10 +6,12 @@ BUILDDIR="./output"
 
 GIT="$(which git)"
 REPODIR="mesa-git"
-# REPO=(--branch radv-float8-hack3 https://gitlab.freedesktop.org/DadSchoorse/mesa.git)
-REPO=(https://gitlab.freedesktop.org/mesa/mesa.git)
-# REV="7485541cc3a8a4f60ef66e02265048aadf14b3ed" # pin to 25.1.1 (busted)
-REV="97f71420dfdf86de084b64cbcbd65855063fcc94" # pin to current upstream
+REPO=(--branch radv-float8-hack3 https://gitlab.freedesktop.org/DadSchoorse/mesa.git)
+REV="fa81930282c218e87232643937418bb2a1ca15bd" # pin to the last-known-good
+
+# REPO=(https://gitlab.freedesktop.org/mesa/mesa.git)
+# REV="7485541cc3a8a4f60ef66e02265048aadf14b3ed" # pin to 25.1.1
+# REV="ea73edde07bd858473c1e256f5dc9a0ecfb351ce" # pin to current upstream
 
 # prevent script from being run outside the project directory
 script_dir="$(dirname "$(readlink -f "$0")")"
@@ -22,20 +24,25 @@ if [ -n "$GIT" ] && ! [ -d ./"$REPODIR" ]; then
 	"$GIT" clone "${REPO[@]}" "$REPODIR"
 	cd "$REPODIR" || exit 1
 	# reset repo to pinned revision state
-	"$GIT" reset --hard "$REV"
-	"$GIT" clean -fd
-	# apply radv-float8-hack3 patch from DadSchoorse:
-	# https://gitlab.freedesktop.org/DadSchoorse/mesa/-/commit/4823285a2e5b2df849b55861dd4f051dd2598bf1
-	if ! "$GIT" am --no-gpg-sign ../radv-float8-hack3.patch; then
-		# if ! "$GIT" apply <../radv-float8-hack3.patch; then
-		echo "Something went wrong when applying 'radv-float8-hack3.patch'!"
-		exit 1
-	else
+	if "$GIT" reset --hard "$REV" && "$GIT" clean -fd; then
 		cd "$script_dir" || exit 1
+	else
+		echo "Something went wrong when resetting WIP repo!"
+		exit 1
 	fi
+
+	# USE THE BELOW IF PULLING FROM UPSTREAM MESA REPO
+	# apply radv-float8-hack3 patch from DadSchoorse's branch:
+	# https://gitlab.freedesktop.org/DadSchoorse/mesa/-/commits/radv-float8-hack3
+	# if ! "$GIT" am --no-gpg-sign ../radv-float8-hack3.patch; then
+	# 	# 	# if ! "$GIT" apply <../radv-float8-hack3.patch; then
+	# 	echo "Something went wrong when applying 'radv-float8-hack3.patch'!"
+	# 	exit 1
+	# else
+	# 	cd "$script_dir" || exit 1
+	# fi
 fi
 
-chmod 0755 ./entry.sh
 mkdir -p "$BUILDDIR"
 
 if podman build -t "$CNAME" .; then
