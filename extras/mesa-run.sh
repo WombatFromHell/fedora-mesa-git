c95575ad15e0a02847076560b065073824324187
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-06-27 21:22:15 -0600

    Refactor mesa-run.sh for newer FSR4 workflow

--- extras/mesa-run.sh
+++ extras/mesa-run.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 MESA="$HOME/mesa"
-ARGS=("DXIL_SPIRV_CONFIG=wmma_fp8_hack")
 
-# D3D_MODULE_PATH="$MESA"/lib64/d3d/d3dadapter9.so.1 \ # Gallium related (deprecated)
-LD_LIBRARY_PATH="$MESA"/lib64:"$LD_LIBRARY_PATH" \
-	VK_ICD_FILENAMES="$MESA"/share/vulkan/icd.d/radeon_icd.x86_64.json \
-	VK_DRIVER_FILES="$MESA"/share/vulkan/icd.d/radeon_icd.x86_64.json \
-	DRIRC_CONFIGDIR="$MESA"/share/drirc.d \
-	LIBGL_DRIVERS_PATH="$MESA"/lib64/dri \
-	LIBVA_DRIVERS_PATH="$MESA"/lib64/dri \
-	VDPAU_DRIVER_PATH="$MESA"/lib64/dri \
-	exec env "${ARGS[@]}" "$@"
+# assume we're using OptiScaler (with FSR4_UPGRADE as a fallback)
+ARGS=(
+	# "DXIL_SPIRV_CONFIG=wmma_rdna3_workaround" # uncomment for RDNA3
+	"PROTON_FSR4_UPGRADE=1"
+	"WINEDLLOVERRIDES=d3d12=n,b,dxgi=n,b,winmm=n,b,nvapi64=n,b"
+)
+ENV_VARS=(
+	"LD_LIBRARY_PATH=$MESA/lib64:$LD_LIBRARY_PATH"
+	"VK_ICD_FILENAMES=$MESA/share/vulkan/icd.d/radeon_icd.x86_64.json"
+	"VK_DRIVER_FILES=$MESA/share/vulkan/icd.d/radeon_icd.x86_64.json"
+	"DRIRC_CONFIGDIR=$MESA/share/drirc.d"
+	"LIBGL_DRIVERS_PATH=$MESA/lib64/dri"
+	"LIBVA_DRIVERS_PATH=$MESA/lib64/dri"
+	"VDPAU_DRIVER_PATH=$MESA/lib64/dri"
+)
+[[ ${#ARGS[@]} -gt 0 ]] && ENV_VARS+=("${ARGS[@]}")
+
+exec env "${ENV_VARS[@]}" "$@"
