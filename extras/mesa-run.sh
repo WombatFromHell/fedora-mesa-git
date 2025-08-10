d3e719887b26e392409491181598d73f0e802739
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-08-10 08:23:59 -0600

    Include llvmpipe for gbm compatibility

--- extras/mesa-run.sh
+++ extras/mesa-run.sh
@@ -4,20 +4,21 @@ MESA="$HOME/mesa"
 
 # assume we're using OptiScaler (with FSR4_UPGRADE as a fallback)
 ARGS=(
-	# "DXIL_SPIRV_CONFIG=wmma_rdna3_workaround" # uncomment for RDNA3
-	"PROTON_FSR4_UPGRADE=1"
-	"WINEDLLOVERRIDES=dxgi,winmm=n,b"
-	"radv_cooperative_matrix2_nv=false"
+  # "DXIL_SPIRV_CONFIG=wmma_rdna3_workaround" # uncomment for RDNA3
+  "PROTON_FSR4_UPGRADE=1"
+  "WINEDLLOVERRIDES=dxgi,winmm=n,b"
+  "radv_cooperative_matrix2_nv=false"
 )
 ENV_VARS=(
-	"LD_LIBRARY_PATH=$MESA/lib64:$LD_LIBRARY_PATH"
-	"VK_ICD_FILENAMES=$MESA/share/vulkan/icd.d/radeon_icd.x86_64.json"
-	"VK_DRIVER_FILES=$MESA/share/vulkan/icd.d/radeon_icd.x86_64.json"
-	"VK_ADD_IMPLICIT_LAYER_PATH=$MESA/share/vulkan/implicit_layer.d"
-	"DRIRC_CONFIGDIR=$MESA/share/drirc.d"
-	"LIBGL_DRIVERS_PATH=$MESA/lib64/dri"
-	"LIBVA_DRIVERS_PATH=$MESA/lib64/dri"
-	"VDPAU_DRIVER_PATH=$MESA/lib64/dri"
+  "LD_LIBRARY_PATH=$MESA/lib64:$LD_LIBRARY_PATH"
+  "VK_ICD_FILENAMES=$MESA/share/vulkan/icd.d/radeon_icd.x86_64.json"
+  "VK_DRIVER_FILES=$MESA/share/vulkan/icd.d/radeon_icd.x86_64.json"
+  "VK_ADD_IMPLICIT_LAYER_PATH=$MESA/share/vulkan/implicit_layer.d"
+  "DRIRC_CONFIGDIR=$MESA/share/drirc.d"
+  "GBM_BACKENDS_PATH=$MESA/lib64/gbm"
+  "LIBGL_DRIVERS_PATH=$MESA/lib64/dri"
+  "LIBVA_DRIVERS_PATH=$MESA/lib64/dri"
+  "VDPAU_DRIVER_PATH=$MESA/lib64/dri"
 )
 [[ ${#ARGS[@]} -gt 0 ]] && ENV_VARS+=("${ARGS[@]}")
 
